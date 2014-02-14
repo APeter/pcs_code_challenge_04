220f29a3d4959e75f5c32c5cb5547f3f44c30af7
AJ Peter <APeter7@gmail.com>
2014-02-13 20:18:53 -0800

    Initial Commit
    Starting with data from code challenge 3

---
+++ txt2csv/lib/parse_names.rb
@@ -0,0 +1,20 @@
+
+class Parse
+
+  def self.parse_names (prefixes, suffixes, name_string)
+
+    parsed_name = {pre:"", first:"", middle:"", last:"", suffix:""}
+
+    # get the last word and see if it's a suffix
+    # if so, save as suffix and store the next to last word as last_name
+    # otherwise store last word as last name
+
+    word = name_string.split
+    parsed_name[:suffix] = word.pop if suffixes.include? word.last
+    parsed_name[:pre] = word.shift if prefixes.include? word.first
+    parsed_name[:last] = word.pop 
+    parsed_name[:first] = word.shift unless word == []
+    parsed_name[:middle] = word.pop unless word == []
+    parsed_name.values
+  end
+end
