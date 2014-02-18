eea5cbc4eb9ab0342948506eecba316e87aed4af
AJ Peter <APeter7@gmail.com>
2014-02-18 14:55:54 -0800

    Rubocop
    Updated Parse.rb to be compliant within rubocop standards

--- txt2csv/lib/parse.rb
+++ txt2csv/lib/parse.rb
@@ -1,9 +1,9 @@
 
 class Parse
-
-  def self.parse_names (prefixes, suffixes, name_string)
-
-    parsed_name = {pre:"", first:"", middle:"", last:"", suffix:""}
+# will parse information from files for names, phone numbers,
+# twitter ID's and email addresses
+  def self.parse_names(prefixes, suffixes, name_string)
+    parsed_name = { pre: '', first: '', middle: '', last: '', suffix: '' }
 
     # get the last word and see if it's a suffix
     # if so, save as suffix and store the next to last word as last_name
@@ -12,31 +12,27 @@ class Parse
     word = name_string.split
     parsed_name[:suffix] = word.pop if suffixes.include? word.last
     parsed_name[:pre] = word.shift if prefixes.include? word.first
-    parsed_name[:last] = word.pop 
+    parsed_name[:last] = word.pop
     parsed_name[:first] = word.shift unless word == []
     parsed_name[:middle] = word.pop unless word == []
     parsed_name.values
   end
 
-  def self.parse_numbers (phone_string)
-
-    parsed_number = {country_code:"", area_code:"", prefix:"", line:"", extension:""}
-
+  def self.parse_numbers(phone_string)
+    parsed_number = { country_code: '', area_code: '', prefix: '', line: '', extension: '' }
     number = phone_string.split(/\W/)
-    number.reject!{ |i| i.empty? }
-    
+    number.reject! { |i| i.empty? }
     case number.size
-
     when 5
+      parsed_number[:country_code] = number.shift
+    when 4
+      if number.last.include?('x')
+        parsed_number[:country_code] = ''
+      else
         parsed_number[:country_code] = number.shift
-    when 4 
-       if number.last.include?("x") 
-            parsed_number[:country_code] = "" 
-        else 
-            parsed_number[:country_code] = number.shift 
-        end
+      end
     else
-        nil
+      nil
     end
 
     parsed_number[:area_code] = number.shift
@@ -44,23 +40,22 @@ class Parse
     parsed_number[:line] = number.shift
     parsed_number[:extension] = number.shift if number.size != 0
     parsed_number.values
-    end
+  end
 
-    def self.parse_email (email_string)
-
-        if email_string.match(/\w+[@]\w+[.]\w+/)
-            parsed_email = [email_string]
-        else
-            parsed_email = ["Not Found"] 
-        end
+  def self.parse_email(email_string)
+    if email_string.match(/\w+[@]\w+[.]\w+/)
+      parsed_email = [email_string]
+    else
+      parsed_email = ["Not Found"] 
     end
+  end
 
-    def self.parse_twitter (twitter_string)
-        if twitter_string.include?("@")
-            twitter_string[0] = ''
-            parsed_twitter = [twitter_string]
-        else
-            parsed_twitter = [twitter_string]
-        end
+  def self.parse_twitter(twitter_string)
+    if twitter_string.include?('@')
+      twitter_string[0] = ''
+      parsed_twitter = [twitter_string]
+    else
+      parsed_twitter = [twitter_string]
     end
+  end
 end
